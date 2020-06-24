33132c30071036db674cb1fd2d5f60b963220ea6
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-06-24 18:00:02 +0900

    料理のshow

--- spec/system/dishes_spec.rb
+++ spec/system/dishes_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Dishes", type: :system do 
     let!(:user) { create(:user) }
+    let!(:dish) { create(:dish, user: user) }
 
     describe "料理登録ページ" do 
-        before do 
-            login_for_system(user)
-            visit new_dish_path
-        end
-
         context "ページレイアウト" do 
-            it "「料理登録」の文字列が存在すること" do 
-                expect(page).to have_content '料理登録'
+            before do 
+                login_for_system(user)
+                visit dish_path(dish)
             end
 
             it "正しいタイトルが表示されること" do 
-                expect(page).to have_title full_title('料理登録')
+                expect(page).to have_title full_title("#{dish.name}")
             end
+
+            it "料理情報が表示されること" do 
+                expect(page).to have_content dish.name
+                expect(page).to have_content dish.description
+                expect(page).to have_content dish.portion
+                expect(page).to have_content dish.tips
+                expect(page).to have_content dish.reference
+                expect(page).to have_content dish.required_time
+                expect(page).to have_content dish.popularity
+            end
+                
         end
     end
 end
