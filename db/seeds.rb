c11657e67ce5bf98e66e6b64822cc23f46e65dd1
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-06-26 18:25:20 +0900

    recruit page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(name:  "山田 太郎",
+User.create!(name:  "採用担当者様（Admin）",
     email: "sample@example.com",
     password:              "foobar",
     password_confirmation: "foobar",
@@ -14,6 +14,9 @@ User.create!(name:  name,
       password_confirmation: password)
 end
 
+
+
+
 10.times do |n|
     Dish.create!(name: Faker::Food.dish,
                  description: "冬に食べたくなる、身体が温まる料理です",
@@ -23,8 +26,53 @@ end
                  required_time: 30,
                  popularity: 5,
                  cook_memo: "初めて作った割にはうまくできた！",
+                 picture: open("#{Rails.root}/public/images/sample.jpg"),
                  user_id: 1)
   end
+
+Dish.create!(name: "鶏胸肉のステーキ",
+    description: "調理簡単！高タンパク質低脂質のマッチョの味方",
+    portion: 1.5,
+    tips: "さっと電子レンジでチンして少し蒸すこと",
+    reference: "https://cookpad.com/recipe/2798655",
+    required_time: 10,
+    popularity: 5,
+    cook_memo: "リピート確実",
+    picture: open("#{Rails.root}/public/images/steak.jpg"),
+    user_id: 1)
+
+Dish.create!(name: "豆乳シリアル",
+    description: "忙しい朝にぴったり",
+    portion: 1.0,
+    tips: "豆乳もしくは無脂肪牛乳がおすすめ",
+    reference: "https://cookpad.com/recipe/2798655",
+    required_time: 2,
+    popularity: 4,
+    cook_memo: "飲みすぎて食欲のない朝にもぴったり",
+    picture: open("#{Rails.root}/public/images/dish2.jpg"),
+    user_id: 1)
+
+Dish.create!(name: "ブロッコリーサラダ",
+    description: "野菜不足もこれで解消",
+    portion: 1.0,
+    tips: "ツナ缶などでタンパク質を加えるとBetter!",
+    reference: "https://cookpad.com/recipe/2798655",
+    required_time: 5,
+    popularity: 3,
+    cook_memo: "ドレッシングは塩が低カロリーで良い！",
+    picture: open("#{Rails.root}/public/images/dish3.jpg"),
+    user_id: 1)
+
+Dish.create!(name: "サーモンのグリル",
+    description: "タンパク質と良質な脂質もGetだぜ",
+    portion: 1.0,
+    tips: "トラウトサーモンは脂質が多いので食べ過ぎに注意すること",
+    reference: "https://cookpad.com/recipe/2798655",
+    required_time: 5,
+    popularity: 3,
+    cook_memo: "お弁当にも最適",
+    picture: open("#{Rails.root}/public/images/dish4.jpg"),
+    user_id: 1)
 
   users = User.all
   user = users.first 
